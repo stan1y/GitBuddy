e2467debebd475172fc9b35c56010529a5a92424
Stanislav Yudin <Stanly@Silverblaze.local>
2010-09-05 01:20:36 +0530

    ObjC interface to python wrapper of /opt/local/bin/git. Python wrapper writes JSON data to stdout at result of specified command arguments. GitWrapper converts it to NSDictionary, NSArray, etc...

---
+++ GitWrapper.h
@@ -0,0 +1,25 @@
+//
+//  GitWrapper.h
+//  GitBuddy
+//
+//  Created by Stanislav Yudin on 4/9/2010.
+//  Copyright 2010 __MyCompanyName__. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+#import "JSON.h"
+
+@interface GitWrapper : NSObject {
+	NSLock * gitLock;
+	NSString * wrapperPath;
+	NSString * gitPath;
+	SBJsonParser *parser;
+	int timeout;
+}
+
+- (id) getCommandJson:(NSArray *)args;
+- (NSDictionary *) getChanges:(NSString *)path;
+- (NSArray *) getBranches:(NSString *)path;
+- (NSArray *) getRemote:(NSString *)path;
+
+@end
---
+++ GitWrapper.m
@@ -0,0 +1,139 @@
+//
+//  GitWrapper.m
+//  GitBuddy
+//
+//  Created by Stanislav Yudin on 4/9/2010.
+//  Copyright 2010 __MyCompanyName__. All rights reserved.
+//
+
+#import "GitWrapper.h"
+
+@implementation GitWrapper
+
+- (id) init
+{
+	if ( !(self = [super init]) ) {
+		return nil;
+	}
+	
+	gitLock = [[NSLock alloc] init];
+	parser = [[SBJsonParser alloc] init];
+	wrapperPath = [[NSBundle mainBundle] pathForResource:@"wrapper" ofType:@"py"];
+	[wrapperPath retain];
+	
+	NSUserDefaults *defaults = [NSUserDefaults standardUserDefaults];
+	gitPath = [defaults stringForKey:@"gitPath"];
+	timeout = [defaults integerForKey:@"gitTimeout"];
+	
+	NSLog(@"Git at %@", gitPath);
+	NSLog(@"Git wrapper at %@", wrapperPath);
+	
+	return self;
+}
+
+- (id) getCommandJson:(NSArray *)args
+{
+	NSMutableArray * _args = [NSMutableArray arrayWithArray:args];
+	[_args insertObject:wrapperPath atIndex:0];
+	[_args insertObject:[NSString stringWithFormat:@"--git=%@", gitPath] atIndex:1];
+	
+	NSPipe *stdoutPipe = [[[NSPipe alloc] init] autorelease];
+	NSPipe *stderrPipe = [[[NSPipe alloc] init] autorelease];
+	NSTask *gitWrapper = [[[NSTask alloc] init] autorelease];
+	
+	[gitWrapper setLaunchPath:@"/usr/bin/python"];
+	[gitWrapper setArguments:_args];
+	
+	[gitWrapper setStandardInput:[NSFileHandle fileHandleWithNullDevice]];
+	[gitWrapper setStandardError:stderrPipe];
+	[gitWrapper setStandardOutput:stdoutPipe];
+	
+	NSLog(@"GitWrapper: /usr/bin/python %@ %@", wrapperPath, [_args componentsJoinedByString:@" "]);
+	int totalSlept = 0;
+	[gitWrapper launch];
+	
+	while (YES) {
+		if (totalSlept >= timeout) {
+			[gitWrapper terminate];
+			NSRunAlertPanel(@"Oups...", [NSString stringWithFormat:@"Git command timed out after %d seconds. Try increasing timeout value in preferences.", timeout], @"Continue", nil, nil);
+			return nil;
+		}
+		sleep(1);
+		
+		if ( ![gitWrapper isRunning] ) {
+			break;
+		}
+		totalSlept++;
+	}
+	
+	if ( [gitWrapper terminationReason] != NSTaskTerminationReasonExit) {
+		NSRunAlertPanel(@"Oups...", @"Git command terminated with unknown status. Something went really wrong.", @"Continue", nil, nil);
+		[gitWrapper release];
+		return nil;
+	}
+	else {
+		int rc = [gitWrapper terminationStatus];
+		if (rc != 0) {
+			NSString * errorMessage = [[NSString alloc] initWithData:[[stderrPipe fileHandleForReading] readDataToEndOfFile] encoding:NSUTF8StringEncoding];
+			NSRunAlertPanel(@"Oups...", [NSString stringWithFormat:@"Git wrapper failed with code %d. %@", rc, errorMessage], @"Continue", nil, nil);
+			
+			return nil;
+		}
+		
+		//read output and parse 
+		NSString * output = [[[NSString alloc] initWithData:[[stdoutPipe fileHandleForReading] readDataToEndOfFile] encoding:NSUTF8StringEncoding] autorelease];
+		NSError * err = nil;
+		id jsonObj = [parser objectWithString:output error:&err];
+		if (err) {
+			[[NSApplication sharedApplication] presentError:err];
+			return nil;
+		}
+		if (jsonObj) {
+			NSLog(@"getCommandJson -> [%@] %@", [jsonObj class], jsonObj);
+			return jsonObj;
+		}
+		else {
+			NSLog(@"getCommandJson -> nil");
+			return nil;
+		}
+
+	}
+}
+
+//	--- Wrapper API	---
+
+- (NSArray *) getBranches:(NSString *)path
+{
+	//FIXME
+	return [NSArray arrayWithObject:@"master"];
+}
+
+- (NSArray *) getRemote:(NSString *)path
+{
+	//FIXME 
+	return [NSArray arrayWithObject:@"origin"];
+}
+
+- (NSDictionary *) getChanges:(NSString *)path
+{
+	// /usr/bin/python wrapper.py --status <path>
+	NSMutableArray *args = [NSMutableArray array];
+	[args addObject:@"--status"];
+	[args addObject:path];
+	
+	id jsonObj = [self getCommandJson:args];
+	if (jsonObj ) {
+		if ([jsonObj isKindOfClass:[NSDictionary class]])
+			return jsonObj;
+		else {
+			NSLog(@"getChanges error: unexpected json received [%@]: %@", [jsonObj class], jsonObj);
+			return nil;
+		}
+	}
+	else {
+		NSLog(@"getChanges error: no json received.");
+		return nil;
+	}
+}
+
+@end
