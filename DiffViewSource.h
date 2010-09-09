d6cae3cb4e473fd612e8b2a5470a7214da4b89c9
Stanislav Yudin <Stanly@Silverblaze.local>
2010-09-10 01:51:49 +0530

    ""Rename ChangesSource to DiffViewSource""

---
+++ DiffViewSource.h
@@ -0,0 +1,33 @@
+//
+//  DiffViewSource.h
+//  GitBuddy
+//
+//  Created by Stanislav Yudin on 8/9/2010.
+//  Copyright 2010 __MyCompanyName__. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//	Class implements table view source with
+//	diff contents either from:
+//	 * git show KEY
+//	 * git diff /repo/file/path
+@interface DiffViewSource :  NSObject<NSTableViewDataSource> {
+	NSMutableDictionary *gitObjectsIndex;
+	NSDictionary *currentSource;
+	
+	NSTableView *tableView;
+	NSProgressIndicator *indicator;
+}
+
+//assigned from nib
+@property (assign) IBOutlet NSTableView *tableView;
+@property (assign) IBOutlet NSProgressIndicator *indicator;
+@property (retain, readonly) NSMutableDictionary *gitObjectsIndex;
+- (void) rebuildIndex:(NSString *)projectPath withCompletionBlock:(void (^)(void))codeBlock;
+
+- (void) updateWithChangeset:(NSString *)filePath inPath:(NSString *)projectPath;
+- (void) updateWithFileDiff:(NSString *)filePath inPath:(NSString *)projectPath;
+
+- (NSString *) stringAtIndex:(int)index;
+@end
---
+++ DiffViewSource.m
@@ -0,0 +1,134 @@
+//
+//  DiffViewSource.m
+//  GitBuddy
+//
+//  Created by Stanislav Yudin on 8/9/2010.
+//  Copyright 2010 __MyCompanyName__. All rights reserved.
+//
+
+#import "DiffViewSource.h"
+#import "GitWrapper.h"
+
+@implementation DiffViewSource
+
+@synthesize tableView, indicator, gitObjectsIndex;
+
+- (id) init
+{
+	if ( !(self = [super init])) {
+		return nil;
+	}
+	
+	gitObjectsIndex = nil;
+	
+	return self;
+}
+
+- (void) dealloc
+{
+	[gitObjectsIndex release];
+	[currentSource release];
+	[super dealloc];
+}
+
+- (void) rebuildIndex:(NSString *)projectPath withCompletionBlock:(void (^)(void))codeBlock
+{
+	NSString * repoArg = [NSString stringWithFormat:@"--repo=%@", projectPath];
+	NSLog(@"Quering staged contents at %@...", projectPath);
+	[tableView setEnabled:NO];
+	[indicator setHidden:NO];
+	[indicator startAnimation:nil];
+	//scan remote, branch and changes
+	GitWrapper * wrapper = [GitWrapper sharedInstance];
+	[wrapper executeGit:[NSArray arrayWithObjects:@"--staged-index", repoArg, nil] withCompletionBlock: ^(NSDictionary *dict){
+		[indicator stopAnimation:nil];
+		[indicator setHidden:YES];
+		
+		if (gitObjectsIndex) {
+			[gitObjectsIndex release];
+		}
+		if (dict) {
+			gitObjectsIndex = [[NSMutableDictionary alloc] init];
+			for(int i=0; i < [[dict objectForKey:@"count"] intValue] / 2; i++) {
+				[gitObjectsIndex setObject:[[dict objectForKey:@"keys"] objectAtIndex:i] forKey:[[dict objectForKey:@"files"] objectAtIndex:i]];
+			}
+			NSLog(@"Git Stage Index");
+			NSLog(@"%@", gitObjectsIndex);
+			NSLog(@"***");
+			
+			[tableView setEnabled:YES];
+			//call block
+			codeBlock();
+		}
+	}];
+}
+
+- (void) updateWithChangeset:(NSString *)filePath inPath:(NSString *)projectPath
+{
+	//scan remote, branch and changes
+	NSString * repoArg = [NSString stringWithFormat:@"--repo=%@", projectPath];
+	NSString * showArg = [NSString stringWithFormat:@"--show=%@", [gitObjectsIndex objectForKey:filePath] ];
+	
+	[tableView setEnabled:NO];
+	[indicator setHidden:NO];
+	[indicator startAnimation:nil];
+	GitWrapper * wrapper = [GitWrapper sharedInstance];
+	[wrapper executeGit:[NSArray arrayWithObjects:showArg, repoArg, nil] withCompletionBlock: ^(NSDictionary *dict){
+		if (currentSource) {
+			[currentSource release];
+		}
+		
+		[indicator stopAnimation:nil];
+		[indicator setHidden:YES];
+		[tableView setEnabled:YES];
+		
+		NSLog(@"Reloading changes with changeset");
+		currentSource = dict;
+		[currentSource retain];
+		[tableView reloadData];
+	}];
+}
+
+- (void) updateWithFileDiff:(NSString *)filePath inPath:(NSString *)projectPath
+{
+	NSString * repoArg = [NSString stringWithFormat:@"--repo=%@", projectPath];
+	NSString * diffArg = [NSString stringWithFormat:@"--diff=%@", filePath];
+	
+	[tableView setEnabled:NO];
+	[indicator setHidden:NO];
+	[indicator startAnimation:nil];
+	
+	GitWrapper * wrapper = [GitWrapper sharedInstance];
+	[wrapper executeGit:[NSArray arrayWithObjects:diffArg, repoArg, nil] withCompletionBlock: ^(NSDictionary *dict){
+		if (currentSource) {
+			[currentSource release];
+		}
+		
+		[indicator stopAnimation:nil];
+		[indicator setHidden:YES];
+		[tableView setEnabled:YES];
+		
+		NSLog(@"Reloading changes with file diff");
+		currentSource = dict;
+		[currentSource retain];
+		[tableView reloadData];
+	}];
+	
+}
+
+- (NSInteger)numberOfRowsInTableView:(NSTableView *)aTableView
+{
+	return [[currentSource objectForKey:@"lines"] count];
+}
+
+- (NSString *) stringAtIndex:(int)index
+{
+	return [[currentSource objectForKey:@"lines"] objectAtIndex:index];
+}
+
+- (id)tableView:(NSTableView *)aTableView objectValueForTableColumn:(NSTableColumn *)aTableColumn row:(NSInteger)rowIndex
+{
+	return [self stringAtIndex:rowIndex];
+}
+
+@end
