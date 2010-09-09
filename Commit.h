e10fc18cbde93ac7795c9c700018638d0c7e5bde
Stanislav Yudin <Stanly@Silverblaze.local>
2010-09-10 00:46:16 +0530

    ""Add Commit panel implementation""

---
+++ Commit.h
@@ -0,0 +1,38 @@
+//
+//  Commit.h
+//  GitBuddy
+//
+//  Created by Stanislav Yudin on 9/9/2010.
+//  Copyright 2010 __MyCompanyName__. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+#import "ProjectFilesSource.h"
+
+@interface Commit : NSWindowController {
+	
+	NSTableView *filesView;
+	ProjectFilesSource *stagedSource;
+	NSTextField *commitMessage;
+	NSString *projectPath;
+	NSButton *previewBtn;
+	
+	NSString *selectedFile;
+}
+
+- (void) commitProject:(NSDictionary*)proj atPath:(NSString*)path;
+
+@property (retain) NSString *projectPath;
+@property (retain) NSString *selectedFile;
+
+//assigned from nib
+@property (assign) IBOutlet NSButton *previewBtn;
+@property (assign) IBOutlet NSTableView *filesView;
+@property (assign) IBOutlet NSTextField *commitMessage;
+@property (assign) IBOutlet ProjectFilesSource *stagedSource;
+
+- (IBAction) fileSelected:(id)sender;
+- (IBAction) showPreview:(id)sender;
+- (IBAction) commit:(id)sender;
+
+@end
---
+++ Commit.m
@@ -0,0 +1,70 @@
+//
+//  Commit.m
+//  GitBuddy
+//
+//  Created by Stanislav Yudin on 9/9/2010.
+//  Copyright 2010 __MyCompanyName__. All rights reserved.
+//
+
+#import "Commit.h"
+#import "GitWrapper.h"
+#import "GitBuddy.h"
+
+@implementation Commit
+
+@synthesize stagedSource, filesView, previewBtn;
+@synthesize commitMessage;
+@synthesize selectedFile, projectPath;
+
+- (void) commitProject:(NSDictionary*)proj atPath:(NSString*)path
+{
+	[self setProjectPath:path];
+	[stagedSource loadProjectData:[proj objectForKey:@"staged"] forPath:path];
+	[filesView reloadData];
+}
+
+- (IBAction) fileSelected:(id)sender
+{
+	int index = [filesView selectedRow];
+	if (index == -1) {
+		NSLog(@"Deselected.");
+		[previewBtn setHidden:YES];
+	}
+	[self setSelectedFile:[stagedSource fileAtIndex:index]];
+	NSLog(@"Selected file %@", [self selectedFile]);
+	[previewBtn setHidden:NO];
+}
+
+- (IBAction) showPreview:(id)sender
+{
+	[[(GitBuddy*)[NSApp delegate] preview] loadPreviewOf:[self selectedFile] inPath:[self projectPath]];
+	[[(GitBuddy*)[NSApp delegate] preview] showWindow:sender];
+}
+
+- (IBAction) commit:(id)sender
+{
+	//check message
+	NSString *msg = [commitMessage stringValue];
+	if ([msg length] == 0) {
+		if (NSRunAlertPanel(@"Empty Commit Log Message!", @"Are you sure that your commit should be with no message?", 
+							@"Yes", @"No", nil) != 1) {
+			//cancel commit
+			return;
+		}
+	}
+	
+	//commit files
+	NSArray *files = [[stagedSource data] objectForKey:@"staged"];
+	GitWrapper* wrapper = [GitWrapper sharedInstance];
+	NSString *repoArg = [NSString stringWithFormat:@"--repo=%@", projectPath];
+	NSString *commitArg = [NSString stringWithFormat:@"--commit=\"%@\"", msg];
+	[wrapper executeGit:[NSArray arrayWithObjects:repoArg, commitArg, nil] withCompletionBlock:^(NSDictionary *dict){
+		
+		//rescan after commit
+		[(GitBuddy*)[NSApp delegate] rescanRepos:sender];
+	}];
+	
+	[[self window] performClose:sender];
+}
+
+@end
